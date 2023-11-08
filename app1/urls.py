419eadd5b964273fd40e3d1625ce14601aceaf7c
Prasad <prasadprabhudesai11@gmail.com>
2023-11-08 09:51:11 -0800

    Updating URL as per django version 4

--- app1/urls.py
+++ app1/urls.py
@@ -1,14 +1,14 @@
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib import admin
 from django.urls import path
 from app1 import views
 from .views import FileView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$', views.home),
     path(r'upload', views.upload),
     path(r'download', views.download),
     path(r'api', views.FileView.as_view(), name='api'),
-    url(r'files', views.files_list),
+    re_path(r'files', views.files_list),
 ]
